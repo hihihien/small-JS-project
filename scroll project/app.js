d1f1b7faf0ac41a9a01638c8918e119bf081872e
Hien Giang <gianghien2999@gmail.com>
2022-01-19 13:09:39 +0100

    update function for smooth scroll, back to top button and fixed navbar

--- scroll project/app.js
+++ scroll project/app.js
@@ -17,12 +17,45 @@ navToggle.addEventListener("click", function() {
   const linksHeight = links.getBoundingClientRect().height;
 
   if(containerHeight === 0) {
-      linksContainer.getElementsByClassName.height = `${linksHeight}px`;
+      linksContainer.style.height = `${linksHeight}px`;
   } else {
-      linksConatiner.getElementsByClassName.height = 0;
+      linksContainer.style.height = 0;
   }
 });
+const navbar = document.getElementById("nav");
+const topLink = document.querySelector(".top-link");
 // ********** fixed navbar ************
+window.addEventListener('scroll', function() {
+  const scrollHeight = window.pageYOffset;
+  const navHeight = navbar.getBoundingClientRect().height;
+  if (scrollHeight > navHeight) {
+    navbar.classList.add('fixed-nav');
+  }
+  else{
+    navbar.classList.remove("fixed-nav");
+  }
 
+if(scrollHeight > 500){
+  topLink.classList.add("show-link");
+} else {
+  topLink.classList.remove("show-link");
+}
+
+}); 
 // ********** smooth scroll ************
+
+
 // select links
+const scrollLinks = document.querySelectorAll(".scroll-link");
+
+scrollLinks.forEach(function(link){
+  link.addEventListener("click", function (e) {
+    // prevent default
+    e.preventDefault();
+    // navigate to specific spot
+    const id = e.currentTarget.getAttibute("href").slice(1);
+    const element = document.getElementById(id);
+    let position = element.offsetTop;
+    
+  });
+});
