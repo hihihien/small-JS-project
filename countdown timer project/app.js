d697921d8fd15562c87c444d44ddace4b6841030
Hien Giang <gianghien2999@gmail.com>
2022-02-06 02:54:24 +0100

    set timer

--- countdown timer project/app.js
+++ countdown timer project/app.js
@@ -21,3 +21,44 @@ const months = [
     "Friday",
     "Saturday",
   ];
+
+const giveaway = document.querySelector(".giveaway");
+const deadline = document.querySelector(".deadline");
+const items = document.querySelectorAll(".deadline-format h4");
+
+let tempDate = new Date(2020);
+let tempeYear = tempDate.getFullYear();
+let tempMonth = tempDate.getMonth();
+let tempDay = tempDate.getDate();
+// month are ZERO index based;
+const futureDate = new Date(tempYear, tempMonth, tempDay + 10,11,30, 0);
+// console.log(futureDate);
+
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+let month = futureDate.getMonth();
+month = months[month];
+const weekday = weekdays[futureDate.getDay()];
+const date = futureDate.getDate();
+giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:§{minutes}am`;
+
+const futureTime = futureDate.getTime();
+function getRemaindingTime() {
+  const today = new Date().getTime();
+
+  const t = futureTime - today;
+  // 1s = 1000ms
+  // 1m = 60s
+  // 1hr = 60 m 
+  // 1d = 24hr
+  // values in miliseconds
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60* 1000;
+  const oneMinute = 60 * 1000;
+
+  // calculate all values
+  
+}
